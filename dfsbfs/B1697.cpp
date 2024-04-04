cc09f7350743c94316b0a0dda85989e958329499
nagool <quietwave00@gmail.com>
2024-04-04 23:44:29 +0900

    fix B1697 BFS 틀린 풀이로 잘못 올림

--- dfsbfs/B1697.cpp
+++ dfsbfs/B1697.cpp
@@ -10,48 +10,47 @@
 using namespace std;
 
 int N, K; // 수빈이, 동생
-int cnt = 0;
-vector<bool> visited(MAX);
+// int cnt = 0;
+vector<bool> graph(MAX);
 queue<pair<int, int>> q;
 
 void BFS(int x) {
-    visited[N] = true;
-    q.push(make_pair(x, cnt));
+    q.push(make_pair(x, 0));
 
     while(!q.empty()) {
         int cur_x = q.front().first;
-        cnt = q.front().second;
+        int cnt = q.front().second;
         q.pop();
 
-        cout << "cur_x: " << cur_x << endl;
+        // cout << "cur_x: " << cur_x << endl;
         if(cur_x == K) {
             cout << cnt << "\n";
             break;
         }
 
-        if(cur_x < MAX && cur_x > 0) {
-            int next_x = cur_x + 1;
-            cout << "next_x: " << next_x << endl;
+        
 
-            if(visited[next_x] == false) {
-                visited[next_x] = true;
-                q.push(make_pair(next_x, cnt++));
-            }
-
-            int prev_x = cur_x - 1;
-            cout << "prev_x: " << prev_x << endl;
-            if(visited[prev_x] == false) {
-                visited[prev_x] = true;
-                q.push(make_pair(prev_x, cnt++));
-            }
-
-            int double_x = cur_x * 2; 
-            cout << "double_x: " << double_x << endl;
-            if(visited[double_x] == false) {
-                visited[double_x] = true;
-                q.push(make_pair(double_x, cnt++));
-            }
+        int prev_x = cur_x - 1;
+        // cout << "prev_x: " << prev_x << endl;
+        if(prev_x >= 0 && graph[prev_x] == false) {
+            graph[prev_x] = true;
+            q.push(make_pair(prev_x, cnt + 1));
         }
+        
+        int next_x = cur_x + 1;
+        // cout << "next_x: " << next_x << endl;
+
+        if(next_x <= K && graph[next_x] == false) {
+            graph[next_x] = true;
+            q.push(make_pair(next_x, cnt + 1));
+        }
+
+        int double_x = cur_x * 2; 
+        // cout << "double_x: " << double_x << endl;
+        if(double_x <= K + 1 && graph[double_x] == false) {
+            graph[double_x] = true;
+            q.push(make_pair(double_x, cnt + 1));
+        }    
     }
 }
 
@@ -61,7 +60,7 @@ int main() {
 	cout.tie(NULL);
 
     cin >> N >> K;
-
     BFS(N);
+
     return 0;
 }
